b892004b5af778737cf6023595f189342c338347
sai kumar <dasaraju1835@gmail.com>
2021-03-22 11:46:18 +0530

    Spring boot

---
+++ SpringMvcTask/src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,59 @@
+package com.example.demo;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.models.Alien;
+
+@Controller
+public class HomeController {
+	
+	@ModelAttribute
+	public void Modeldata(Model m) {
+		m.addAttribute("name", "sai kumar");
+	}
+	
+	@RequestMapping("/")
+	public String home() {
+		System.out.println("nice");
+		return "index";
+		
+	}
+	
+	@RequestMapping("add")
+	public ModelAndView add(@RequestParam("num1") int i,@RequestParam("num2") int j) {
+		ModelAndView mv=new ModelAndView();
+		mv.setViewName("result");
+		int k=i+j;
+		mv.addObject("result", k);
+		
+		return mv;
+		
+	}
+	
+	@RequestMapping("addAlien")
+	public String addAlien(@ModelAttribute("result") Alien a) {
+		return "result";
+		
+	}
+	
+	/*
+	 * @RequestMapping("add") public String add(HttpServletRequest req) { int
+	 * i=Integer.parseInt(req.getParameter("num1")); int
+	 * j=Integer.parseInt(req.getParameter("num2")); int k=i+j;
+	 * 
+	 * HttpSession s=req.getSession(); s.setAttribute("result", k);
+	 * 
+	 * return "result.jsp";
+	 * 
+	 * }
+	 */
+	}
---
+++ SpringMvcTask/src/main/webapp/views/index.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+Welcome to my world
+<form action="add">
+<input type="number" name="num1" placeholder="enter 1st number" ><br>
+<input type="number" name="num2" placeholder="enter 2nd number" ><br>
+<input type="submit">
+
+
+
+</form>
+<br><br>
+
+<form action="addAlien">
+<input type="number" name="id" placeholder="enter your id" ><br>
+<input type="text" name="name" placeholder="enter your name" ><br>
+<input type="submit">
+
+
+
+</form>
+</body>
+</html>
